e804c17307a69ef7a67b9ea1f9c14eec072436a2
NikeshKhatiwada <nikeshkhatiwada@tutanota.com>
2023-06-25 21:07:17 +0545

    Added Video Model

---
+++ Models/Video.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoDirectory_Server.Models
+{
+    public class Video
+    {
+        [Key, Required]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Url { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string MainFilePath { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+    }
+}
