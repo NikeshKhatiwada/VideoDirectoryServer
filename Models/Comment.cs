c3641b9eae443d4218817b0bf529575e23c7d1dd
NikeshKhatiwada <nikeshkhatiwada@tutanota.com>
2023-06-25 21:28:21 +0545

    Added Comment Model

---
+++ Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoDirectory_Server.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public User User { get; set; }
+        [Required]
+        public Video Video { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+    }
+}
