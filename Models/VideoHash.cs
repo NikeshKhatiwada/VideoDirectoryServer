23ba29572e6a3a905f1944ef6ae01563f39f26e9
NikeshKhatiwada <nikeshkhatiwada@tutanota.com>
2023-10-19 23:52:02 +0545

    Added model for video IPFS hashes

---
+++ Models/VideoHash.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+
+namespace VideoDirectory_Server.Models
+{
+    public class VideoHash
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public Video Video { get; set; }
+        [Required]
+        public string Resolution { get; set; }
+        [Required]
+        public string Hash { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+    }
+}
