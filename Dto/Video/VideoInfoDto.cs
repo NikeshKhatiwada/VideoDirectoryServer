9784bffc38d9a7126622f1c4fecf0f6035370db7
NikeshKhatiwada <nikeshkhatiwada@tutanota.com>
2023-10-19 23:50:40 +0545

    Added video info dto to deal with video addition and update

---
+++ Dto/Video/VideoInfoDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoDirectory_Server.Dto.Video
+{
+    public class VideoInfoDto
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public List<string> Tags { get; set; }
+        [Required]
+        public string Image { get; set; }
+        [Required]
+        public string ImageExtension { get; set; }
+        [Required]
+        public string ChannelUrl { get; set; }
+    }
+}
