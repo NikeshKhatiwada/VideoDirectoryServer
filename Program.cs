3542cabdd7577d56bd3606f4a367ac91513f7d51
NikeshKhatiwada <nikeshkhatiwada@tutanota.com>
2023-10-19 23:31:25 +0545

    Updated packages and program

--- Program.cs
+++ Program.cs
@@ -1,14 +1,63 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using VideoDirectory_Server.Data;
+using VideoDirectory_Server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configuration setup
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var secretKeyString = builder.Configuration.GetValue<string>("Key:SecretKey");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 
-// Add services to the container.
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKeyString)) // Replace with your own secret key
+        };
+    });
+
+//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+//    .AddCookie(options =>
+//    {
+//        options.Cookie.Name = "MyAppAuthCookie";
+//        options.LoginPath = "/Account/Login";
+//        options.AccessDeniedPath = "/Account/AccessDenied";
+//    });
+
+//builder.Services.AddSession(option =>
+//{
+//    option.IdleTimeout = TimeSpan.FromHours(24);
+//    option.Cookie.HttpOnly = true;
+//    option.Cookie.IsEssential = true;
+
+//});
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+builder.Services.AddSingleton<AuthTokenGenerator>();
+builder.Services.AddSingleton<VideoUrlGenerator>();
+builder.Services.AddSingleton<InitialVideoEncodingService>();
+builder.Services.AddSingleton<VideoEncodingAndPublishingService>();
+builder.Services.AddSingleton<VideoEditingService>();
+builder.Services.AddSingleton<VideoFilteringService>();
+builder.Services.AddSingleton<VideoTranscriptionService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -26,7 +75,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
+//app.UseSession();
 
 app.MapControllers();
 
