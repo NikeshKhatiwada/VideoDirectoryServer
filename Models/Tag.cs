92b32fcab3f211f8c13c524c087686c0b5ed0205
NikeshKhatiwada <nikeshkhatiwada@tutanota.com>
2023-10-19 23:56:08 +0545

    Added model for video tags

---
+++ Models/Tag.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoDirectory_Server.Models
+{
+    public class Tag
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public ICollection<AssociatedVideoTag> AssociatedVideoTags { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+
+        public Tag()
+        {
+            AssociatedVideoTags = new List<AssociatedVideoTag>();
+        }
+    }
+}
