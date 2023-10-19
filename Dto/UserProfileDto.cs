915196a12a8fa0a5d9372cae18a708908e963741
NikeshKhatiwada <nikeshkhatiwada@tutanota.com>
2023-10-19 23:51:10 +0545

    Added DTOs for user profile update

---
+++ Dto/UserProfileDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoDirectory_Server.Dto
+{
+    public class UserProfileDto
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
---
+++ Dto/UserProfileImageDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoDirectory_Server.Dto
+{
+    public class UserProfileImageDto
+    {
+        [Required]
+        public byte[] ImageBytes { get; set; }
+        [Required]
+        public string ImageExtension { get; set; }
+    }
+}
