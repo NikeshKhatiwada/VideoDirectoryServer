d67820fdac5d1870a748e3e71155e769cebd2b3f
NikeshKhatiwada <nikeshkhatiwada@tutanota.com>
2023-10-19 23:52:58 +0545

    Added model for system administrator

---
+++ Models/SystemAdmin.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoDirectory_Server.Models
+{
+    public class SystemAdmin
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+    }
+}
