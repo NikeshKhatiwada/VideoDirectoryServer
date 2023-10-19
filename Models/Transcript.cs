79e86f53b18e82f96f5dc486a9f44c54241fcaed
NikeshKhatiwada <nikeshkhatiwada@tutanota.com>
2023-10-19 23:52:28 +0545

    Added model for video transcript

---
+++ Models/Transcript.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoDirectory_Server.Models
+{
+    public class Transcript
+    {
+        [Key, Required]
+        public int VideoId { get; set; }
+        public Video Video { get; set; }
+        [Required]
+        public string Language { get; set; }
+        [Required]
+        public string Content { get; set; }
+    }
+}
