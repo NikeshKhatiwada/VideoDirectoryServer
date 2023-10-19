e5734b2d5fdac04b6d8cd70cb668f14526136b9f
NikeshKhatiwada <nikeshkhatiwada@tutanota.com>
2023-10-19 23:34:30 +0545

    Added Video Url Generator to generate unique urls while uploading videos

---
+++ Services/VideoUrlGenerator.cs
@@ -0,0 +1,31 @@
+﻿namespace VideoDirectory_Server.Services
+{
+    public class VideoUrlGenerator
+    {
+        public string GenerateUniqueShortUrl()
+        {
+            Guid guid = Guid.NewGuid();
+            string base64Guid = Convert.ToBase64String(guid.ToByteArray());
+
+            base64Guid = base64Guid.Replace("/", "_").Replace("+", "-");
+
+            int desiredLength = 10;
+            string shortUrl = base64Guid.Substring(0, desiredLength);
+
+            while (IsShortUrlAlreadyExists(shortUrl))
+            {
+                guid = Guid.NewGuid();
+                base64Guid = Convert.ToBase64String(guid.ToByteArray());
+                base64Guid = base64Guid.Replace("/", "_").Replace("+", "-");
+                shortUrl = base64Guid.Substring(0, desiredLength);
+            }
+
+            return shortUrl;
+        }
+
+        private static bool IsShortUrlAlreadyExists(string shortUrl)
+        {
+            return false;
+        }
+    }
+}
