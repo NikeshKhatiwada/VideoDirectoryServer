c805aa3e01916f0de7bab5d3052eed5929dbf7f5
NikeshKhatiwada <nikeshkhatiwada@tutanota.com>
2023-10-19 23:55:50 +0545

    Added DTOs for video edit and filters

---
+++ Dto/Video/VideoEditingDto.cs
@@ -0,0 +1,13 @@
+﻿namespace VideoDirectory_Server.Dto.Video
+{
+    public class VideoEditingDto
+    {
+        public string VideoUrl { get; set; }
+        public TimeSpan TrimFrom { get; set; }
+        public TimeSpan TrimTo { get; set; }
+        public int RotationOrFlipValue { get; set; }
+        public int AspectRatioNumerator { get; set; }
+        public int AspectRatioDenominator { get; set; }
+        public string TextLogo { get; set; }
+    }
+}
---
+++ Dto/Video/VideoFilteringDto.cs
@@ -0,0 +1,12 @@
+﻿namespace VideoDirectory_Server.Dto.Video
+{
+    public class VideoFilteringDto
+    {
+        public string VideoUrl { get; set; }
+        public char GrayscaleOrSepiaType { get; set; }
+        public char ColorTintType { get; set; }
+        public int BrightnessValue { get; set; }
+        public int ContrastValue { get; set; }
+        public int SaturationValue { get; set; }
+    }
+}
