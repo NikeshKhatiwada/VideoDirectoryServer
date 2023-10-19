245eb42deb0edefa53fe72ba88d3062182c7b76d
NikeshKhatiwada <nikeshkhatiwada@tutanota.com>
2023-10-19 23:33:47 +0545

    Added DTOs for user registration and login

---
+++ Dto/UserLoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoDirectory_Server.Dto
+{
+    public class UserLoginDto
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ Dto/UserRegistrationDto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoDirectory_Server.Dto
+{
+    public class UserRegistrationDto
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public byte[] SelectedImage { get; set; }
+        [Required]
+        public string SelectedImageExtension { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
