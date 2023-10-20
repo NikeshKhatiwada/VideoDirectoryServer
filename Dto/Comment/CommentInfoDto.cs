714c03f1602820037f9d6214c3eea70200d54d52
NikeshKhatiwada <nikeshkhatiwada@tutanota.com>
2023-10-20 07:41:27 +0545

    Added dto for video comments

---
+++ Dto/Comment/CommentInfoDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoDirectory_Server.Dto.Comment
+{
+    public class CommentInfoDto
+    {
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string VideoUrl { get; set; }
+    }
+}
