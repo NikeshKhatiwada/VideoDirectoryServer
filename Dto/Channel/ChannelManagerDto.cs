ab1c1a09c145d3f67d830b1bd441f1c58a3d1e42
NikeshKhatiwada <nikeshkhatiwada@tutanota.com>
2023-10-20 07:39:07 +0545

    Added Dto for Channel managers (admins or owners)

---
+++ Dto/Channel/ChannelManagerDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoDirectory_Server.Dto.Channel
+{
+    public class ChannelManagerDto
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string ChannelUrl { get; set;  }
+    }
+}
