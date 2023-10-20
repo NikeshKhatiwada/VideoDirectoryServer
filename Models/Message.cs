3697a44fd5b7214e0b972358b6d7937b9e3aba8b
NikeshKhatiwada <nikeshkhatiwada@tutanota.com>
2023-10-20 07:47:40 +0545

    Added model for chat messages

---
+++ Models/Message.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoDirectory_Server.Models
+{
+    public class Message
+    {
+        [Key]
+        public int Id { get; set; }
+        public Guid SenderId { get; set; }
+        public User Sender { get; set; }
+        public Guid ReceiverId { get; set; }
+        public User Receiver { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+    }
+}
