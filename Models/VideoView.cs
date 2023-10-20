983deac6d8e7e3da8ca90b501d5206c787521322
NikeshKhatiwada <nikeshkhatiwada@tutanota.com>
2023-10-20 07:46:11 +0545

    Added model for views of video

---
+++ Models/VideoView.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoDirectory_Server.Models
+{
+    public class VideoView
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public User User { get; set; }
+        [Required]
+        public Video Video { get; set; }
+        [Required]
+        public int ViewCount { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+    }
+}
