8ed907c0abe1064ef608ef08c9783ad6dd9c966a
NikeshKhatiwada <nikeshkhatiwada@tutanota.com>
2023-10-20 07:41:57 +0545

    Added dto for reports about video, channel and comment

---
+++ Dto/Report/ReportInfoDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoDirectory_Server.Dto.Report
+{
+    public class ReportInfoDto
+    {
+        [Required]
+        public string Reason { get; set; }
+        [Required]
+        public char ContentType { get; set; }
+        [Required]
+        public string ContentIdentifier { get; set; }
+    }
+}
