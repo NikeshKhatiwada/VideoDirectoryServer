8e11fbf27c9ad211a2eeb24a1a109867dc8af92c
NikeshKhatiwada <nikeshkhatiwada@tutanota.com>
2023-10-20 07:56:53 +0545

    Added model for reports

---
+++ Models/Report.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoDirectory_Server.Models
+{
+    public class Report
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public User ReporterUser { get; set; }
+        [Required]
+        public string Reason { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+    }
+
+    public class VideoReport : Report
+    {
+        public int VideoId { get; set; }
+        public Video Video { get; set; }
+    }
+
+    public class CommentReport : Report
+    {
+        public int CommentId { get; set; }
+        public Comment Comment { get; set; }
+    }
+
+    public class UserReport : Report
+    {
+        public Guid ReportedUserId { get; set; }
+        public User ReportedUser { get; set; }
+    }
+
+    public class ChannelReport : Report
+    {
+        public Guid ChannelId { get; set; }
+        public Channel Channel { get; set; }
+    }
+}
