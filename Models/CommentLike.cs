865655943cfe697d066a4a3b149f863455efe1a8
NikeshKhatiwada <nikeshkhatiwada@tutanota.com>
2023-10-20 07:39:39 +0545

    Added model to deal with and record comment likes

---
+++ Models/CommentLike.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoDirectory_Server.Models
+{
+    public class CommentLike
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public User User { get; set; }
+        [Required]
+        public Comment Comment { get; set; }
+        [Required]
+        public bool LikeDislike { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+    }
+}
